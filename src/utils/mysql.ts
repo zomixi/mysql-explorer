5e284388a9f4359588a289a929349519fa2a1843
zomixi <zomixi@qq.com>
2022-08-19 00:13:18 +0800

    chore: add mysql util

---
+++ src/utils/mysql.ts
@@ -0,0 +1,27 @@
+import { Connection, createConnection } from "mysql";
+
+const connect = (
+  host: string,
+  port: string,
+  userName: string,
+  password: string
+) => {
+  return new Promise<Connection>((resolve, reject) => {
+    const connection = createConnection({
+      host: host,
+      port: Number(port),
+      user: userName,
+      password: password,
+    });
+
+    connection.connect((error) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(connection);
+      }
+    });
+  });
+};
+
+export { connect };
